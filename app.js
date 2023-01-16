343414fe954050612a0a6ad4c93a026531e98e13
laurynas <unextbusinessman@gmail.com>
2023-01-16 11:40:40 +0100

    new object, functions and map

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 var arr = ["apa", "orangutang", "gorilla", "get", "giraff"];
 console.log(arr);
+var obj = {
+    name: "ape",
+    age: 3,
+    animal: true
+};
+console.log(obj);
+var func = function (string) {
+    console.log("hi", string);
+};
+func("x");
+arr.map(function (i) { return console.log(i); });
--- app.ts
+++ app.ts
@@ -1,3 +1,25 @@
 const arr: string[] = ["apa", "orangutang", "gorilla", "get", "giraff"];
 
 console.log(arr);
+
+interface Object {
+  name: string;
+  age: number;
+  animal: boolean;
+}
+
+const obj: Object = {
+  name: "ape",
+  age: 3,
+  animal: true,
+};
+
+console.log(obj);
+
+const func = (string: string): void => {
+  console.log("hi", string);
+};
+
+func("x");
+
+arr.map((i: string) => console.log(i));
